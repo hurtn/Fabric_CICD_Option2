53f6db418ad98bd82937b83dd2546dde39358ccb
System Administrator <admin@MngEnvMCAP553100.onmicrosoft.com>
2025-04-13 15:45:37 +0000

    Updated deploy_to_fabric.py

--- scripts/deploy/deploy_to_fabric.py
+++ scripts/deploy/deploy_to_fabric.py
@@ -1,20 +1,15 @@
-import os
+import os,argparse
 from fabric_cicd import FabricWorkspace, publish_all_items, unpublish_all_orphan_items,change_log_level
+
+parser = argparse.ArgumentParser(description='Process Azure Pipeline arguments.')
+parser.add_argument('--workspace_id', type=str)
+
+args = parser.parse_args()
 
 change_log_level("DEBUG")
 # Get the environment variable
 branch = os.getenv("BUILD_SOURCEBRANCH").replace("refs/heads/","")
-print(f'Branch: {branch}')
-
-# Define branch-to-environment mapping
-if branch == "dev":
-    workspace_id = "e489926a-747f-444d-88f7-353222a68892"
-elif branch == "test":
-    workspace_id = "15f88951-a30a-4b7a-9e54-067654c41e03"
-elif branch == "prod":
-    workspace_id = "0c3ab123-5aae-4a03-bafe-0fe00f46e213"
-else:
-    raise ValueError("Invalid branch for deployment: {branch}")
+print(f'Deploying to workspace {branch}')
 
 # Sample values for FabricWorkspace parameters
 environment = branch
@@ -23,7 +18,7 @@ item_type_in_scope = ["Notebook", "DataPipeline", "Lakehouse","SemanticModel","R
 
 # Initialize the FabricWorkspace object with the required parameters
 target_workspace = FabricWorkspace(
-    workspace_id=workspace_id,
+    workspace_id=args.workspace_id,
     environment=environment,
     repository_directory=repository_directory,
     item_type_in_scope=item_type_in_scope,
