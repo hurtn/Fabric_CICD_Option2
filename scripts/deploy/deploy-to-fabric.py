8b00da58c23dd81425c58ebe251d6111a4a6cc5d
System Administrator <admin@MngEnvMCAP553100.onmicrosoft.com>
2025-04-13 21:12:56 +0000

    Updated deploy-to-fabric.py

--- scripts/deploy/deploy-to-fabric.py
+++ scripts/deploy/deploy-to-fabric.py
@@ -75,7 +75,7 @@ else:
     print(f"Workspace ID for {workspace_name} set to {workspace_id}")
 
 # set repo folder
-repository_directory = os.environ["gitFolder"]
+repository_directory = os.environ["GITFOLDER"]
 
 item_type_in_scope = ["Notebook", "DataPipeline", "Lakehouse","SemanticModel","Report"]
 
