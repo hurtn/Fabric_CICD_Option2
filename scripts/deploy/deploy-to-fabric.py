8b0deaed798ba22878541cd807c60e9cb2ed1ecb
System Administrator <admin@MngEnvMCAP553100.onmicrosoft.com>
2025-04-13 20:38:05 +0000

    Updated deploy-to-fabric.py

--- scripts/deploy/deploy-to-fabric.py
+++ scripts/deploy/deploy-to-fabric.py
@@ -59,7 +59,7 @@ print(f'Workspace set to {ws_name}')
 # define workspace name to be deployed to based on value in variable group based on branch name
 workspace_name = os.environ[ws_name.upper()]
 print(f'Obtaining GUID for {workspace_name}')
-scope='https://api.fabric.microsoft.com'
+scope='https://management.azure.com/.default'
 lookup_response = get_workspace_id(workspace_name, token_credential.get_token(scope))
 if lookup_response.startswith("Error"):
     raise ValueError(f"Error detected: {lookup_response}. Perhaps workspace name is set incorrectly in the variable group of does not map to branch name + 'WorkspaceName'")
