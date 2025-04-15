22bcc2938dc1ea22a508f3275277cf4074fb88e5
System Administrator <admin@MngEnvMCAP553100.onmicrosoft.com>
2025-04-15 14:44:08 +0000

    Committing 5 items from workspace fe754f24-794c-47f8-ad94-6323fa305bb7

---
+++ fabric/hello_world.Notebook/notebook-content.py
@@ -0,0 +1,32 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "dd8acc3f-8345-4477-91f8-0598dbc6e9ff",
+# META       "default_lakehouse_name": "Lakehouse",
+# META       "default_lakehouse_workspace_id": "57b33177-c9cb-4bd1-9899-68e7194667e7",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "dd8acc3f-8345-4477-91f8-0598dbc6e9ff"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+print('hello world')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
